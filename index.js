f178186d38421396e90ff14e5cb021386d9acc8c
Denys Dovhan <email@denysdovhan.com>
2016-09-01 23:16:59 +0300

    Implement package.

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
-function emojifyTextAsync(input) {
-  if (typeof input !== 'string') {
-    return Promise.reject(new TypeError(`\input\` should be \`String\`, got \`${typeof input}\``));
-  }
-  return Promise.resolve(input);
-}
+import {
+  __,
+  always,
+  defaultTo,
+  has,
+  ifElse,
+  join,
+  map,
+  pipe,
+  prop,
+  replace,
+  split,
+  toUpper,
+  transpose
+} from 'ramda'
 
-function emojifyText(input) {
-  if (typeof input !== 'string') {
-    throw new TypeError(`\input\` should be \`String\`, got \`${typeof input}\``);
-  }
-  return input;
-}
+import characters from './characters'
 
-export { emojifyText, emojifyTextAsync };
+/**
+ * Convert boring text to wonderful emojis.
+ *
+ * @param  {String}  background              Character to print as background
+ * @param  {String}  foreground              Character to print as foreground
+ * @param  {String}  bg                      Alias for background
+ * @param  {String}  fg                      Alias for foreground
+ * @param  {JSON}    [dictionary=characters] Custom dictionary for rendering
+ * @param  {boolean} [row=false]             Print in single row or not
+ * @param  {String}  text                    Text to process through transformer
+ * @return {String}                          The result of processing
+ */
+export default function emojifyText({
+  bg,
+  fg,
+  background,
+  foreground,
+  dictionary = characters,
+  row = false
+}, text) {
+  return pipe(
+    toUpper(),
+    split(''),
+    map(
+      ifElse(
+        has(__, dictionary),
+        prop(__, dictionary),
+        always(dictionary.nochar)
+      )
+    ),
+    map(
+      map(join(''))
+    ),
+    ifElse(
+      always(row),
+      pipe(transpose(), map(join(''))),
+      map(join('\n'))
+    ),
+    join('\n'),
+    replace(/0/g, defaultTo(defaultTo('0', background), bg)),
+    replace(/1/g, defaultTo(defaultTo('1', foreground), fg))
+  )(text)
+}
