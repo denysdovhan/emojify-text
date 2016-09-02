262e37c2eabad57efe78a8f19afb5f540e4af277
Denys Dovhan <email@denysdovhan.com>
2016-09-02 14:22:20 +0300

    Add tests.

--- test.js
+++ test.js
@@ -1,31 +1,78 @@
-import test from 'tape';
-import { emojifyText, emojifyTextAsync } from './index';
+import { readFileSync } from 'fs'
+import test from 'tape'
+import emojifyText from './index'
 
 test('emojifyText', ({ equal, throws, end }) => {
-  equal(emojifyText('unicorns'), 'unicorns', 'basic');
+  const fixture = readFileSync('fixtures/column-pure.txt', 'utf8').trim()
 
-  throws(() => { emojifyText(); }, TypeError, 'empty input');
-  throws(() => { emojifyText(2); }, TypeError, 'invalid input');
-  end();
-});
+  equal(emojifyText({}, 'test'), fixture, 'basic transformation')
 
-test('async :: basic', ({ equal, end }) => {
-  emojifyTextAsync('unicorns').then(result => {
-    equal(result, 'unicorns');
-    end();
-  });
-});
+  throws(() => { emojifyText({}) }, TypeError, 'empty input')
+  throws(() => { emojifyText({}, []) }, TypeError, 'invalid input')
 
-test('async :: empty input', ({ equal, end }) => {
-  emojifyTextAsync().catch(result => {
-    equal(result instanceof TypeError, true);
-    end();
-  });
-});
+  end()
+})
 
-test('async :: invalid input', ({ equal, end }) => {
-  emojifyTextAsync(2).catch(result => {
-    equal(result instanceof TypeError, true);
-    end();
-  });
-});
+test('characters', ({ equal, end }) => {
+  const fixture = readFileSync('fixtures/column.txt', 'utf8').trim()
+
+  equal(emojifyText({
+    background: '⭐️',
+    foreground: '💙'
+  }, 'test'), fixture, 'background with foreground')
+
+  equal(emojifyText({
+    bg: '⭐️',
+    foreground: '💙'
+  }, 'test'), fixture, 'bg with foreground')
+
+  equal(emojifyText({
+    background: '⭐️',
+    fg: '💙'
+  }, 'test'), fixture, 'background with fg')
+
+  equal(emojifyText({
+    bg: '⭐️',
+    fg: '💙'
+  }, 'test'), fixture, 'bg with fg')
+
+  end()
+})
+
+test('row', ({ equal, end }) => {
+  const fixture = readFileSync('fixtures/row.txt', 'utf8').trim()
+
+  equal(emojifyText({
+    background: '⭐️',
+    foreground: '💙',
+    row: true
+  }, 'test'), fixture, 'render in row')
+
+  end()
+})
+
+test('dictionary', ({ equal, end }) => {
+  const fixture = readFileSync('fixtures/dictionary.txt', 'utf8').trim()
+
+  equal(emojifyText({
+    dictionary: {
+      A: [
+        [ 0, 1, 0 ],
+        [ 1, 0, 1 ],
+        [ 1, 0, 1 ]
+      ],
+      C: [
+        [ 1, 1, 1 ],
+        [ 1, 0, 0 ],
+        [ 1, 1, 1 ]
+      ],
+      Y: [
+        [ 1, 0, 1 ],
+        [ 1, 0, 1 ],
+        [ 0, 1, 0 ]
+      ]
+    }
+  }, 'ACY'), fixture, 'render with custom dictionary')
+
+  end()
+})
